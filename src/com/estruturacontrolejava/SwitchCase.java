b956ae775bcf2fd7e0929837a248fc465438bf7f
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2025-03-28 09:52:26 -0300

    switch case new!

--- src/com/estruturacontrolejava/SwitchCase.java
+++ src/com/estruturacontrolejava/SwitchCase.java
@@ -12,43 +12,14 @@ public class SwitchCase {
         int option = scn.nextInt();
 
         switch(option) {
-            case 1:
-
-                System.out.println("Domingo");
-                break;
-
-            case 2:
-
-                System.out.println("Segunda");
-                break;
-
-            case 3:
-
-                System.out.println("Terça");
-                break;
-
-            case 4:
-
-                System.out.println("Quarta");
-                break;
-
-            case 5:
-
-                System.out.println("Quinta");
-                break;
-
-            case 6:
-
-                System.out.println("Sexta");
-                break;
-
-            case 7:
-
-                System.out.println("Sábado");
-                break;
-
-            default:
-                System.out.println("Opção invalida");
+            case 1 -> System.out.println("Domingo");
+            case 2 -> System.out.println("Segunda");
+            case 3 -> System.out.println("Terça");
+            case 4 -> System.out.println("Quarta");
+            case 5 -> System.out.println("Quinta");
+            case 6 -> System.out.println("Sexta");
+            case 7 -> System.out.println("Sábado");
+            default -> System.out.println("Opção invalida");
         }
 
         scn.close();

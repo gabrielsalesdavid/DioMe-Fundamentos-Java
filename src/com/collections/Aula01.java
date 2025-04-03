63214a85a8965f42000c90b248ed0475a6aa1954
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2025-04-03 17:28:02 -0300

    List search

--- src/com/collections/Aula01.java
+++ src/com/collections/Aula01.java
@@ -1,33 +1,43 @@
 package com.collections;
 
+import java.time.Duration;
+import java.time.OffsetDateTime;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Vector;
 
 public class Aula01 {
 
     public static void main(String[] args) {
 
-        List<User> users = new ArrayList<>();
-        User user = new User(1, "João");
-        users.add(user);
-        users.add(new User(2, "Maria"));
-        users.add(new User(3, "Leo"));
-        System.out.println(users.contains(user));
-        System.out.println(users.size());
-        System.out.println(users.isEmpty());
-        System.out.println(users.getFirst());
-        System.out.println(users.get(0));
-        System.out.println(users.getLast());
-        System.out.println(users.size() - 1);
-        System.out.println(users.contains(new User(6, "Carlos")));
-        System.out.println(new User(4, "Roberto"));
-        System.out.println(new User(5, "Izaac"));
-        System.out.println(new User(1, "Jão").equals(new User(1, "Jão")));
+        List<Integer> arrayList = new ArrayList<>();
 
-        System.out.print(user);
-        System.out.println(users.remove(new User(10, "Leo")));
-        System.out.println(users.remove(0));
-        users.clear();
-        System.out.println(users);
+        var arrayStart = OffsetDateTime.now();
+        for(int i = 0; i < 100_00_000; i++) {
+
+            arrayList.add(i);
+        }
+
+        System.out.println(Duration.between(arrayStart, OffsetDateTime.now()).toMillis());
+
+        var vectorStart = OffsetDateTime.now();
+        List<Integer> vector = new Vector<>();
+        for(int i = 0; i < 100_00_000; i++) {
+
+            vector.add(i);
+        }
+
+        System.out.println(Duration.between(arrayStart, OffsetDateTime.now()).toMillis());
+
+        List<Integer> linkedList = new LinkedList<>();
+
+        var linkedStart = OffsetDateTime.now();
+        for(int i = 0; i < 100_00_000; i++) {
+
+            arrayList.add(i);
+        }
+
+        System.out.println(Duration.between(linkedStart, OffsetDateTime.now()).toMillis());
     }
 }
